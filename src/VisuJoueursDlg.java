96d9543c76d15146a7f32ad544c52bff720cbf50
Saman <saman.ismail.pro@gmail.com>
2022-02-16 20:35:15 +0100

    Second

--- src/VisuJoueursDlg.java
+++ src/VisuJoueursDlg.java
@@ -6,7 +6,7 @@ import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
-/*
+/**
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JDialog.java to edit this template
  */
